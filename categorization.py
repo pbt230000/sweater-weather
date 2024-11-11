fd2300d845107ec1c2b32f23a282e2688744478c
Gayathri Jeyaraman <148653375+gayajyn@users.noreply.github.com>
2024-11-10 20:41:44 -0600

    Update categorization algorithm

--- categorization.py
+++ categorization.py
@@ -1,83 +1,86 @@
 categories = [
-    {"id": 1, "name": "Good", "rank": 2},
-    {"id": 2, "name": "Bad", "rank": 3},
-    {"id": 3, "name": "Perfect", "rank": 1},
-    {"id": 4, "name": "Terrible", "rank": 4}
-]
+    {"id": "1", "name": "Good", "rank": 2},
+    {"id": "2", "name": "Bad", "rank": 3},
+    {"id": "3", "name": "Perfect", "rank": 1},
+    {"id": "4", "name": "Terrible", "rank": 4}
+    ]
 
-preferences = [
-        {"id": 1,
-         "type": "secondary",
-         "links": [1],
-         "rank": -1,
-         "category": -1,
-         "factor": "low_temp",
-         "min": 60,
-         "max": 70},
-        {"id": 2,
-         "type": "primary",
-         "links": [1],
-         "rank": 1,
-         "category": 3,
-         "factor": "high_temp",
-         "min": 75,
-         "max": 80},
-        {"id": 3,
+primary_preferences = [
+        {"id": "4",
          "type": "primary",
          "links": [],
          "rank": 2,
-         "category": 1,
+         "category": "1",
          "factor": "high_temp",
          "min": 70,
-         "max": 90}
-]
+         "max": 90},
+        {"id": "2",
+         "type": "primary",
+         "links": ["1"],
+         "rank": 1,
+         "category": "3",
+         "factor": "high_temp",
+         "min": 75,
+         "max": 80}
+        ]
+
+secondary_preferences = [
+    {"id": "1",
+     "type": "secondary",
+     "links": ["2"],
+     "factor": "low_temp",
+     "min": 60,
+     "max": 70}
+    ]
 
 forecasts = [
-    {"date": "11/10/2024 12:00:00 PM", "high_temp": 77, "low_temp": 65},
-    {"date": "11/11/2024 12:00:00 AM", "high_temp": 72, "low_temp": 55}
-]
+    {"date": "11/10/2024 12:00:00 PM", "high_temp": 70, "low_temp": 65},
+    {"date": "11/10/2024 12:00:00 AM", "high_temp": 80, "low_temp": 70},
+    {"date": "11/08/2024 12:00:00 PM", "high_temp": 95, "low_temp": 70}
+    ]
 
 ratings = []
+primary_preferences_ranked = sorted(primary_preferences, key=lambda x: x['rank'])
 
 for f in forecasts:
-    for p in preferences:
-        if p["type"] == "primary":
-            rating_exists = False
-            for r in ratings:
-                if (r.get("factor") == p.get("factor") and r.get("category") is not None):
-                    rating_exists = True
-                    break
-            if rating_exists:
-                continue
-            sec_pref_met = True
-            for link in p["links"]:
-                for p_sec in preferences:
-                    if p_sec["id"] == link and not (p_sec["min"] <= f.get(p_sec["factor"]) <= p_sec["max"]):
-                        sec_pref_met = False
-                        break
-            if sec_pref_met and p["min"] <= f[p.get("factor")] <= p["max"]:
-                ratings.append({
-                    "date": f["date"],
-                    "factor": p["factor"],
-                    "category": p["category"]
-                })
+    for p in primary_preferences_ranked:
+        rating_exists = False
+        for r in ratings:
+            if r.get("date") == f.get("date") and r.get("factor") == p.get("factor") and r.get("category") is not None:
+                rating_exists = True
                 break
-avg_catg = None
-catg_sum = 0
-for r in ratings:
-    for c in categories:
-        if c["id"] == r["category"]:
-            catg_sum += c["rank"]
-            break
-avg_rank = round(catg_sum / len(ratings))
-for c in categories:
-    if c["rank"] == avg_rank:
-        avg_catg = c["id"]
-        break
-ratings.append({
-    "date": f["date"],
-    "factor": "overall",
-    "category": avg_catg
-})
+        if rating_exists:
+            continue
+        secondary_preferences_met = True
+        for link in p["links"]:
+            for sec_p in secondary_preferences:
+                if sec_p["id"] == link and not (sec_p["min"] <= f.get(sec_p["factor"]) <= sec_p["max"]):
+                    secondary_preferences_met = False
+                    break
+        if secondary_preferences_met and p["min"] <= f[p.get("factor")] <= p["max"]:
+            print(p["category"])
+            ratings.append({
+                "date": f["date"],
+                "factor": p["factor"],
+                "category": p["category"]})
+    category_sum = 0
+    category_count = 0
+    for r in ratings:
+        for c in categories:
+            if r.get("date") == f.get("date") and c["id"] == r["category"]:
+                category_sum += c["rank"]
+                category_count += 1
+    overall_category = None
+    if category_count > 0:
+        average_rank = round(category_sum / category_count)
+        for c in categories:
+            if c["rank"] == average_rank:
+                overall_category = c["id"]
+                break
+        ratings.append({
+            "date": f["date"],
+            "factor": "overall",
+            "category": overall_category})
 
-print(ratings)
+for r in ratings:
+    print(r)
